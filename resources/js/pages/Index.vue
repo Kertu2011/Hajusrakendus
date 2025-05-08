a85625777258502a9529e2229d6c8637262ad87e
Kertu2011 <kertu.beljaev@gmail.com>
2025-05-08 22:55:01 +0300

    fix homepage

--- resources/js/pages/Index.vue
+++ resources/js/pages/Index.vue
@@ -1,7 +1,7 @@
 <template>
     <div class="bg-gray-100 dark:bg-gray-900 text-black dark:text-white min-h-screen flex flex-col items-center justify-center antialiased font-sans">
       <h1 class="text-4xl md:text-5xl font-bold mb-8 text-center px-4">
-        Welcome to Your Project Hub!
+        Hajusrakenduste projektid
       </h1>
   
       <div class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-5 gap-6 px-6 max-w-4xl w-full">
@@ -10,7 +10,7 @@
         </Link>
   
         <Link href="/map" class="bg-purple-500 hover:bg-purple-600 dark:bg-purple-700 dark:hover:bg-purple-800 text-white font-semibold py-3 px-6 rounded-lg shadow-md transition duration-300 ease-in-out transform hover:-translate-y-1 text-center block">
-          Kaart (WIP)
+          Kaart
         </Link>
   
         <Link href="/blog" class="bg-green-500 hover:bg-green-600 dark:bg-green-700 dark:hover:bg-green-800 text-white font-semibold py-3 px-6 rounded-lg shadow-md transition duration-300 ease-in-out transform hover:-translate-y-1 text-center block">
